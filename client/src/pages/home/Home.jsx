4672df023a917527016521e87758d954dc29a5c7
tienv1per <tienv1perds@gmail.com>
2023-04-24 16:21:49 +0700

    add componets lessons

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import "./home.css";
 import Header from '../../components/header/Header';
 import Navbar from '../../components/navbar/Navbar';
+import Lessons from '../../components/lessons/Lessons';
 
 const Home = () => {
     return (
         <div className='home'>
             <Navbar/>
             <Header/>
+            <div className='homeContainer'>
+                <Lessons/>
+            </div>
         </div>
     )
 }
