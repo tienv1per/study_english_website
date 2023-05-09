839b69daf63a89f232e796416b9238b2d7e8362d
tienv1per <tienv1perds@gmail.com>
2023-05-09 14:05:13 +0700

    fix cors

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ const connect = async () => {
 }
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // replace with your frontend origin
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // replace with your frontend origin
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
