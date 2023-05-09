82a71923227f2779251e02f5c6a55ef139f2dbe6
tienv1per <tienv1perds@gmail.com>
2023-05-10 01:34:57 +0700

    dit me cuoc doi

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ const connect = async () => {
 }
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', "*"); // replace with your frontend origin
+    res.header('Access-Control-Allow-Origin', `${URL_REACT_APP}`); // replace with your frontend origin
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
